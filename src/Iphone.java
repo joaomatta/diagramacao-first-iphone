12f2644c2f0a2f42968493038357d71992fa49e0
JOAOMATTA <joao.matta@outlook.com>
2024-05-14 08:02:57 -0300

    first commit

---
+++ src/Iphone.java
@@ -0,0 +1,57 @@
+public class Iphone implements AparelhoTelefonico,NavegadorInternet,ReprodutorMusical {
+
+    @Override
+    public void tocar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tocar'");
+    }
+
+    @Override
+    public void pausar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pausar'");
+    }
+
+    @Override
+    public void selecionarMusica() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'selecionarMusica'");
+    }
+
+    @Override
+    public void exibirPagina() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exibirPagina'");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarNovaAba'");
+    }
+
+    @Override
+    public void atualizarPagona() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarPagona'");
+    }
+
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'ligar'");
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atender'");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarCorreioVoz'");
+    }
+ 
+}
